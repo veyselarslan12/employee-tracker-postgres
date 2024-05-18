c1ddadc2b20653f74930d6013f7583ac798a92c1
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-05-18 00:39:13 -0500

    database added

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+\c employee_db;
+
+CREATE TABLE deparment (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    deparment_id INTEGER NOT NULL
+);
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER
+);
---
+++ index.js
@@ -0,0 +1,14 @@
+// QUESTIONS: 
+// View all departments
+// View all roles
+// View all employees
+// Add a department
+// Add a role
+// Add an employee
+// Update an employee role
+// Update employee managers
+// View employees by manager
+// View employees by deparment
+// Delete deparment, roles and employees
+// View the total utilized budget of a department, the combined salaries of all employees in that department
+ 
