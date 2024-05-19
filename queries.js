084445259a74cf9929eee8d83543bb77768ca916
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-05-19 01:41:09 -0500

    add, update and delete employees

--- queries.js
+++ queries.js
@@ -55,23 +55,66 @@ const updateEmployeeRole = async (pool) => {
 };
 
 const addEmployee = async (pool) => {
-    const res = await pool.query(``)
-    console.table(res.rows);
+    const { first_name, last_name } = await inquirer.prompt([
+        {
+            name: 'first_name',
+            type: 'input',
+            message: 'Enter the employee first name:'
+        },
+        {
+            name: 'last_name',
+            type: 'input',
+            message: 'Enter the employee last name:'
+        }
+    ])
+    await pool.query('INSERT INTO employees (first_name, last_name) VALUES ($1, $2)', [first_name, last_name])
+    console.log(`${first_name} ${last_name} added to employees database.`)
 };
 
 const addDepartment = async (pool) => {
-    const res = await pool.query(``)
-    console.table(res.rows);
+    const name = await inquirer.prompt({
+        name: 'name',
+        type: 'input',
+        message: 'Enter the name of the department:',
+    });
+    await pool.query('INSERT INTO departments (name) VALUES ($1)', [name]);
+    console.log(`Added department ${name}`);
 };
 
 const addRole = async (pool) => {
-    const res = await pool.query(``)
-    console.table(res.rows);
+    const department = await pool.query('SELECT * FROM departments')
+    const { title, salary, department_id } = await inquirer.prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'Enter the role title:'
+        },
+        {
+            name: 'salary',
+            type: 'input',
+            message: 'Enter the salary:'
+        },
+        {
+            name: 'department_id',
+            type: 'list',
+            message: 'Select the department for this role:',
+            choices: department.rows.map(departments => ({ name: departments.name, value: department_id}))
+        }
+    ])
+    await pool.query('INSERT INTO roles (title, salary, department_id) VALUES ($1, $2, $3)', [title, salary, department_id])
+    console.log(`Added role ${title}.`);
 };
 
 const deleteDepartment = async (pool) => {
-    const res = await pool.query(``)
-    console.table(res.rows);
+    const department = await pool.query('SELECT * FROM departments')
+    const department_id = await inquirer.prompt({
+        name: 'department_id',
+        type: 'list',
+        message: 'Select the department to delete:',
+        choices: department.rows.map(departments => ({ name: departments.name, value: departments.id}))
+    })
+    await pool.query('DELETE FROM departments WHERE id = $1', [department_id])
+    console.log(`Department deleted.`);
 };
 
 const deleteRoles = async (pool) => {
